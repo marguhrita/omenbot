b91ef0903bd7f07a72a9e88e5b697fd0e3a10902
marguhrita <82453045+marguhrita@users.noreply.github.com>
2021-11-04 16:46:16 +0000

    reminders permissions update

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ async def startup():
 client.loop.create_task(startup())
 
 @client.command(name = "resetreminder")
-@commands.has_role(["botdev","reminders"])
+@commands.has_role(["botdev", "Admin])
 async def resetreminder(ctx):
     if ctx.author.id == 806556994944106587:# or client.get_role(889460025452531780) in ctx.author.roles:
         await db.set("last_reminder", datetime.min.isoformat())
@@ -105,7 +105,7 @@ async def resetreminder(ctx):
         await ctx.send("No.")
 
 @client.command(name = "reminder")
-@commands.has_role(["botdev","reminders"])
+@commands.has_role(["botdev", "Admin])
 async def manual_reminder(ctx, work, time_due, day_due="today" ):
     if ctx.author.id == 806556994944106587:# or client.get_role(889460025452531780) in ctx.author.roles:
         reminderChannel = client.get_channel(898163614312181761)
